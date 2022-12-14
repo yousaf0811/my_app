a1910c959adc29010c09b688f3464ce010a7d08d
yousaf0811 <yousaf0811@gmail.com>
2022-12-14 07:10:03 +0500

    4th commit

--- src/components/students/Student_signUp.js
+++ src/components/students/Student_signUp.js
@@ -1,7 +1,83 @@
 import { useNavigate } from "react-router-dom"
+import { useState } from "react"
 
 const StudentSignUp = () => {
     const navigate = useNavigate();
+    const [firstName, setFirstName] = useState(null);
+    const [lastName, setLastName] = useState(null);
+    const [fatherName, setFatherName] = useState(null);
+    const [address, setAddress] = useState(null);
+    const [gender, setGender] = useState(null);
+    const [dob, setDob] = useState(null);
+    const [state, setState] = useState(null);
+    const [city, setCity] = useState(null);
+    const [course, setCourse] = useState(null);
+    const [email, setEmail] = useState(null);
+    const [password, setPassword] = useState(null);
+    const handleInputChange = (e) => {
+        const { id, value } = e.target;
+        if (id === "firstName") {
+          setFirstName(value);
+        }
+        if (id === "lastName") {
+            setLastName(value);
+          }
+        if (id === "fatherName") {
+          setFatherName(value);
+        }
+        if(id === "address"){
+            setAddress(value);
+        }
+        if(id === "gender"){
+            setGender(value);
+        }
+        if(id === "dob"){
+            setDob(value);
+        }
+        if(id === "state"){
+            setState(value);
+        }
+        if(id === "city"){
+            setCity(value);
+        }
+        if(id === "course"){
+            setCourse(value);
+        }
+        if(id === "email"){
+            setEmail(value);
+        }
+        if(id === "password"){
+            setPassword(value);
+        }
+      };
+
+    const handleClick = () => {
+        navigate('/studentaccount')};
+      const handleSubmit = () => {
+        // console.log(name);
+        if(!firstName ) { alert('Fill the empty input fields');  return false}; 
+        const user = {
+            firstName,
+            lastName,
+            fatherName,
+            address,
+            gender,
+            dob,
+            state,
+            city,
+            course,
+            email,
+            password
+        }
+    localStorage.setItem('name', JSON.stringify(user.firstName));
+        fetch('http://localhost:3010/student',{
+            method : "POST",
+            headers : {
+                "Content-Type" : "application/json"
+            },
+            body : JSON.stringify(user)
+        }).then(resp => resp.json()).then(info => handleClick() );
+      };
     return (
         <div>
         <section class="h-100 bg-dark">
@@ -22,108 +98,80 @@ const StudentSignUp = () => {
                                         <div class="row">
                                             <div class="col-md-6 mb-4">
                                                 <div class="form-outline">
-                                                    <input type="text" id="form3Example1m" class="form-control form-control-lg" />
+                                                    <input onChange={(e) => handleInputChange(e)} type="text" id="firstName" class="form-control form-control-lg" />
                                                     <label class="form-label" for="form3Example1m">First name</label>
                                                 </div>
                                             </div>
                                             <div class="col-md-6 mb-4">
                                                 <div class="form-outline">
-                                                    <input type="text" id="form3Example1n" class="form-control form-control-lg" />
+                                                    <input onChange={(e) => handleInputChange(e)} type="text" id="lastName" class="form-control form-control-lg" />
                                                     <label class="form-label" for="form3Example1n">Last name</label>
                                                 </div>
                                             </div>
                                         </div>
 
                                         <div class="row">
-                                            <div class="col-md-6 mb-4">
+                                            <div class="form-outline mb-4">
                                                 <div class="form-outline">
-                                                    <input type="text" id="form3Example1m1" class="form-control form-control-lg" />
-                                                    <label class="form-label" for="form3Example1m1">Mother's name</label>
-                                                </div>
-                                            </div>
-                                            <div class="col-md-6 mb-4">
-                                                <div class="form-outline">
-                                                    <input type="text" id="form3Example1n1" class="form-control form-control-lg" />
+                                                    <input onChange={(e) => handleInputChange(e)} type="text" id="fatherName" class="form-control form-control-lg" />
                                                     <label class="form-label" for="form3Example1n1">Father's name</label>
                                                 </div>
                                             </div>
                                         </div>
 
                                         <div class="form-outline mb-4">
-                                            <input type="text" id="form3Example8" class="form-control form-control-lg" />
+                                            <input onChange={(e) => handleInputChange(e)} type="text" id="address" class="form-control form-control-lg" />
                                             <label class="form-label" for="form3Example8">Address</label>
                                         </div>
-
-                                        <div class="d-md-flex justify-content-start align-items-center mb-4 py-2">
-
-                                            <h6 class="mb-0 me-4">Gender: </h6>
-
-                                            <div class="form-check form-check-inline mb-0 me-4">
-                                                <input class="form-check-input" type="radio" name="inlineRadioOptions" id="femaleGender"
-                                                    value="option1" />
-                                                <label class="form-check-label" for="femaleGender">Female</label>
-                                            </div>
-
-                                            <div class="form-check form-check-inline mb-0 me-4">
-                                                <input class="form-check-input" type="radio" name="inlineRadioOptions" id="maleGender"
-                                                    value="option2" />
-                                                <label class="form-check-label" for="maleGender">Male</label>
-                                            </div>
-
-                                            <div class="form-check form-check-inline mb-0">
-                                                <input class="form-check-input" type="radio" name="inlineRadioOptions" id="otherGender"
-                                                    value="option3" />
-                                                <label class="form-check-label" for="otherGender">Other</label>
-                                            </div>
-
-                                        </div>
-
                                         <div class="row">
                                             <div class="col-md-6 mb-4">
-
-                                                <select class="select">
-                                                    <option value="1">State</option>
-                                                    <option value="2">Option 1</option>
-                                                    <option value="3">Option 2</option>
-                                                    <option value="4">Option 3</option>
-                                                </select>
-
+                                                <div class="form-outline">
+                                                    <input onChange={(e) => handleInputChange(e)} type="text" id="gender" class="form-control form-control-lg" />
+                                                    <label class="form-label" for="form3Example1m">Gender</label>
+                                                </div>
                                             </div>
                                             <div class="col-md-6 mb-4">
-
-                                                <select class="select">
-                                                    <option value="1">City</option>
-                                                    <option value="2">Option 1</option>
-                                                    <option value="3">Option 2</option>
-                                                    <option value="4">Option 3</option>
-                                                </select>
-
+                                                <div class="form-outline">
+                                                    <input onChange={(e) => handleInputChange(e)} type="date" id="gender" class="form-control form-control-lg" />
+                                                    <label class="form-label" for="form3Example1n">DOB</label>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="row">
+                                            <div class="col-md-6 mb-4">
+                                                <div class="form-outline">
+                                                    <input onChange={(e) => handleInputChange(e)} type="text" id="state" class="form-control form-control-lg" />
+                                                    <label class="form-label" for="form3Example1m">State</label>
+                                                </div>
+                                            </div>
+                                            <div class="col-md-6 mb-4">
+                                                <div class="form-outline">
+                                                    <input onChange={(e) => handleInputChange(e)} type="text" id="city" class="form-control form-control-lg" />
+                                                    <label class="form-label" for="form3Example1n">City</label>
+                                                </div>
                                             </div>
                                         </div>
 
                                         <div class="form-outline mb-4">
-                                            <input type="text" id="form3Example9" class="form-control form-control-lg" />
-                                            <label class="form-label" for="form3Example9">DOB</label>
-                                        </div>
-
-                                        <div class="form-outline mb-4">
-                                            <input type="text" id="form3Example90" class="form-control form-control-lg" />
-                                            <label class="form-label" for="form3Example90">Pincode</label>
-                                        </div>
-
-                                        <div class="form-outline mb-4">
-                                            <input type="text" id="form3Example99" class="form-control form-control-lg" />
+                                            <input onChange={(e) => handleInputChange(e)} type="text" id="course" class="form-control form-control-lg" />
                                             <label class="form-label" for="form3Example99">Course</label>
                                         </div>
 
                                         <div class="form-outline mb-4">
-                                            <input type="text" id="form3Example97" class="form-control form-control-lg" />
+                                            <input onChange={(e) => handleInputChange(e)} type="email" id="email" class="form-control form-control-lg" />
                                             <label class="form-label" for="form3Example97">Email ID</label>
+                                        </div>
+                                        <div class="form-outline mb-4">
+                                            <input onChange={(e) => handleInputChange(e)} type="password" id="password" class="form-control form-control-lg" />
+                                            <label class="form-label" for="form3Example97">Password</label>
                                         </div>
 
                                         <div class="d-flex justify-content-end pt-3">
                                             <button type="button" class="btn btn-light btn-lg">Reset all</button>
-                                            <button type="button" class="btn btn-warning btn-lg ms-2" onClick={()=>{navigate('/studentaccount')}} >Submit form</button>
+                                            <button type="button" class="btn btn-success btn-lg ms-2" onClick={() => handleSubmit()} >Submit form</button>
+                                        </div>
+                                        <div class="d-flex justify-content-end pt-3">
+                                            <button type="button" class="btn btn-success btn-lg ms-4" onClick={()=>{navigate('/studentlogin')}} >Already has Account ? Click for Log in</button>
                                         </div>
 
                                     </div>
